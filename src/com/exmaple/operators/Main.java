2c0165ecc8de01e26fbda01bd8e3e0e28aefa690
raisul1313 <raisul.islam1313@gmail.com>
2021-11-30 13:51:10 +0600

    Initial commit

---
+++ src/com/exmaple/operators/Main.java
@@ -0,0 +1,71 @@
+package com.exmaple.operators;
+
+public class Main {
+    public static void main(String[] args) {
+//        int a = 5;
+//        a = a + 2;
+//        a += 2;
+//        a ++;
+//        a --;
+//        a /=2;
+//        System.out.println(a);
+
+        int a = 2;
+        int b = 3;
+//        boolean answer = a>b;
+//        boolean answer = a<b;
+//        boolean answer = a == b;
+//        boolean answer = a >= b;
+//        boolean answer = a <= b;
+//        boolean answer = a != b;
+//        boolean answer = a== 5 || b == 2;
+//        boolean answer = a== 5 && b == 2;
+//        System.out.println(answer);
+
+//        if(a>3){
+//            System.out.println("A is greater than 3");
+//        }else{
+//            System.out.println("A is not Greater than 3");
+//        }
+
+//       boolean answer = a>3;
+//
+//        if(answer){
+//            System.out.println("A is greater than 3");
+//        }else{
+//            System.out.println("A is not Greater than 3");
+//        }
+//
+//        boolean answer1 = a>3;
+//
+//        if(!answer1){
+//            System.out.println("A is greater than 3");
+//        }else{
+//            System.out.println("A is not Greater than 3");
+//        }
+
+//        if(a>0){
+//            System.out.println("A is Positive");
+//        } else if (a < 0){
+//            System.out.println("A is Negative");
+//        }else{
+//            System.out.println("A is Zero");
+//        }
+
+        switch (a){
+            case 1:
+                System.out.println("A is One");
+                break;
+            case 2:
+                System.out.println("A is Two");
+                break;
+            case 3:
+                System.out.println("A is Three");
+                break;
+            default:
+                System.out.println("A isn't One, Ywo or Three");
+        }
+
+
+    }
+}
